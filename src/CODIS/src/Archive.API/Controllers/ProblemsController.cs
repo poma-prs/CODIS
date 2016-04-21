8005f6e7ba7378f1d5c1539cb57db6703f3472fb
poma.prs <poma.prs@gmail.com>
2016-04-21 15:51:01 +0400

    add problem controller

---
+++ src/CODIS/src/Archive.API/Controllers/ProblemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Archive.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProblemsController : Controller
+    {
+
+    }
+}
