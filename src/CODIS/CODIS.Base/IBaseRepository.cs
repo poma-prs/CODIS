8fffe27fe8f4bd8db3769f4e7ba4dd00c8e82b64
poma.prs <poma.prs@gmail.com>
2016-04-22 10:20:26 +0400

    add IRabbitQueue.cs and IBaseRepository.cs

---
+++ src/CODIS/CODIS.Base/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CODIS.Base
+{
+    public interface IBaseRepository<T>
+    {
+        T Get(long id);
+        T Add(T entry);
+        void Update(T entry);
+        void Delete(T entry);
+    }
+}
---
+++ src/CODIS/CODIS.Base/IRabbitQueue.cs
@@ -0,0 +1,8 @@
+﻿namespace CODIS.Base
+{
+    public interface IRabbitQueue
+    {
+        void Add(Submit submit);
+        SubmitResult GetSubmitResult(long submitId);
+    }
+}
