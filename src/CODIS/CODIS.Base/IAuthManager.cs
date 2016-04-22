b7cdf804e6685aa7b9bd49881a368dba35707599
poma.prs <poma.prs@gmail.com>
2016-04-22 10:17:03 +0400

    add IAuthManager.cs

---
+++ src/CODIS/CODIS.Base/IAuthManager.cs
@@ -0,0 +1,13 @@
+﻿namespace CODIS.Base
+{
+    public class Token
+    {
+        public long UserId { get; set; }
+    }
+
+    public interface IAuthManager
+    {
+        bool CheckToken(string token);
+        Token GetData(string token);
+    }
+}
