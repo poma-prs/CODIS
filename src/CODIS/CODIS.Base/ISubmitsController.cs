354060cc14b1c13f1dcc47d1ccb7b8391e5070ad
poma.prs <poma.prs@gmail.com>
2016-04-22 10:00:00 +0400

    add ISubmitsController.cs

---
+++ src/CODIS/CODIS.Base/ISubmitsController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CODIS.Base
+{
+    public interface ISubmitsController
+    {
+        ICollection<long> Send(ICollection<Submit> submits);
+        void Rejudge(ICollection<long> submitIds);
+    }
+}
