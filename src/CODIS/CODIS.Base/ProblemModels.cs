1d955dd633410540a7d30c91ac5dcb07c76943f5
poma.prs <poma.prs@gmail.com>
2016-04-22 09:53:37 +0400

    add problem models

---
+++ src/CODIS/CODIS.Base/ProblemModels.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CODIS.Base
+{
+    public class Problem
+    {
+        public Dictionary<string, ProblemInfo> Info { get; set; }
+        public Statement Statement { get; set; }
+        public ProblemResourceLimits ResourceLimits { get; set; }
+    }
+
+    public class Statement
+    {
+        public StatementVersion Version { get; set; }
+    }
+
+    public class StatementVersion
+    {
+        public string Language { get; set; }
+        public string Format { get; set; }
+    }
+
+    public class ProblemInfo
+    {
+        public string Name { get; set; }
+    }
+
+    public class ProblemResourceLimits
+    {
+        public long TimeLimitMillis { get; set; }
+        public long MemoryLimitBytes { get; set; }
+    }
+}
