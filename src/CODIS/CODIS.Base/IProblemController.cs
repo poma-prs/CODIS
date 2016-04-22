2c9a988d845767003f2feb9bed8bbb1bf1bbdf8c
poma.prs <poma.prs@gmail.com>
2016-04-22 09:57:04 +0400

    add IProblemController.cs

---
+++ src/CODIS/CODIS.Base/IProblemController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CODIS.Base
+{
+    public interface IProblemController
+    {
+        ICollection<Problem> GetCollection(long collectionId);
+        string GetStatementUrl(long problemId);
+    }
+}
